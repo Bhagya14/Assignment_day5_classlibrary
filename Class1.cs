72744f56082a3ff5df52be5bfcf6ddf67cb22824
Bhagya14 <45635523+Bhagya14@users.noreply.github.com>
2018-12-30 23:50:54 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_assignment
+{
+    public class Class1
+    {
+        public int Getsum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        public int Getmultiply(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+        public int Getdivide(int n1, int n2)
+        {
+            return n1 / n2;
+        }
+        public int Getsubtract(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+    }
+}
